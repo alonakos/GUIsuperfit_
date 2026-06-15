f4e2946fbd5b699a6f6b2f20f783d3392e65bff4
Alyona <alonakosobokova@gmail.com>
2026-06-15 11:37:16 -0700

    Update params.py

--- NGSF/NGSF/params.py
+++ NGSF/NGSF/params.py
@@ -8,30 +8,18 @@ from NGSF.Header_Binnings import kill_header
 
 
 def parseJsonString(myjson):
-    """
-    Check if JSON string
-    return JSON or False
-    """
     try:
-        json.loads(myjson)
+        return json.loads(myjson)
     except ValueError:
         return False
-    return json.loads(myjson)
 
 
 def parseJsonFile(file):
-    """
-    Check if JSON file
-    return JSON or False
-
-    """
     try:
         with open(file, "r") as read_file:
-            data = json.load(read_file)
-            return data
+            return json.load(read_file)
     except Exception:
         return False
-    return json.load(file)
 
 
 data = parseJsonString(sys.argv[1])
@@ -45,10 +33,9 @@ if not data:
 
 class Parameters:
     def __init__(self, data):
-
         self.object_to_fit = data["object_to_fit"]
         self.save_results_path = data["saving_results_path"]
-
+        self.progress_path = data.get("progress_path")
         self.use_exact_z = data["use_exact_z"]
         self.z_exact = data["z_exact"]
         self.z_range_begin = data["z_range_begin"]
@@ -73,10 +60,8 @@ class Parameters:
         self.epoch_high = data["epoch_high"]
         self.epoch_low = data["epoch_low"]
 
-        # Chose minimum overlap
         self.minimum_overlap = data["minimum_overlap"]
 
-        # Number of steps for A_v (do not change)
         self.Alam_high = data["Alam_high"]
         self.Alam_low = data["Alam_low"]
         self.Alam_interval = data["Alam_interval"]
@@ -84,7 +69,6 @@ class Parameters:
         alam_num = int((self.Alam_high - self.Alam_low) / self.Alam_interval) + 1
         self.extconstant = np.linspace(self.Alam_low, self.Alam_high, alam_num)
 
-        # Library to look at
         self.temp_gal_tr = data["temp_gal_tr"]
         self.temp_sn_tr = data["temp_sn_tr"]
 
@@ -93,39 +77,21 @@ class Parameters:
         self.lower = data["lower_lam"]
 
         if self.upper == self.lower:
+            spectrum = kill_header(self.object_to_fit)
+            self.lower = spectrum[0][0] - 300
+            self.upper = spectrum[-1][0] + 300
 
-            self.lower = kill_header(self.object_to_fit)[1][0] - 300
-            self.upper = kill_header(self.object_to_fit)[-1][0] + 300
-
-            interval = int((self.upper - self.lower) / self.resolution)
-            self.lam = np.linspace(self.lower, self.upper, interval)
-
-        else:
-
-            self.upper = data["upper_lam"]
-            self.lower = data["lower_lam"]
-            interval = int((self.upper - self.lower) / self.resolution)
-            self.lam = np.linspace(self.lower, self.upper, interval)
+        interval = int((self.upper - self.lower) / self.resolution)
+        self.lam = np.linspace(self.lower, self.upper, interval)
 
         # Kind of error spectrum ('SG', 'linear' or 'included')
         self.kind = str(data["error_spectrum"]).lower()
 
-        # Show plot?
         self.show = data["show_plot"]
-
-        # Allow png output.
-        if "show_plot_png" in data:
-            self.show_plot_png = data["show_plot_png"]
-        else:
-            self.show_plot_png = False
-
-        # How many results to plot?
+        self.show_plot_png = data.get("show_plot_png", False)
         self.n = data["how_many_plots"]
 
         self.iterations = 10
-
-        # Template library
-
         if self.resolution == 10 or self.resolution == 30:
             templates_gal = glob.glob(
                 "bank/binnings/" + str(self.resolution) + "A/gal/*"
